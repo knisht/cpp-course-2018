efb87b6b7db5db944b74c2e531c48bade41a7b3d
Konstantin Nisht <zeron.n1@yandex.ru>
2018-11-14 22:45:10 +0300

    fixed hash bug

--- HW05/src/core/checker.cpp
+++ HW05/src/core/checker.cpp
@@ -1,5 +1,6 @@
 #include "include/core/checker.hpp"
 #include <algorithm>
+#include <chrono>
 #include <cstring>
 #include <filesystem>
 #include <fstream>
@@ -20,16 +21,17 @@ namespace
 
 namespace fs = std::filesystem;
 // bfs
-std::vector<std::string> get_filenames(std::string const &path)
+std::vector<std::pair<std::string, size_t>>
+get_filenames(std::string const &path)
 {
-    std::vector<std::string> filenames;
+    std::vector<std::pair<std::string, size_t>> filenames;
     std::queue<std::string> queue;
     std::unordered_set<std::string> processed_filenames;
     queue.push(path);
     while (!queue.empty()) {
         try {
             fs::directory_iterator entries(queue.front());
-            for (auto &p : entries) {
+            for (auto &&p : entries) {
                 try {
                     fs::path canonical_path = fs::canonical(p);
                     if (processed_filenames.count(canonical_path) != 0) {
@@ -38,7 +40,8 @@ std::vector<std::string> get_filenames(std::string const &path)
                     if (fs::is_directory(canonical_path)) {
                         queue.push(canonical_path);
                     } else {
-                        filenames.push_back(canonical_path);
+                        filenames.push_back(
+                            {canonical_path, fs::file_size(canonical_path)});
                     }
                     processed_filenames.insert(canonical_path);
                 } catch (fs::filesystem_error &e) {
@@ -53,6 +56,8 @@ std::vector<std::string> get_filenames(std::string const &path)
     return filenames;
 }
 
+size_t hashcheck = 0;
+
 size_t get_hash(std::string const &filepath)
 {
     std::ifstream ifs(filepath, std::ios::binary | std::ios::ate);
@@ -65,35 +70,43 @@ size_t get_hash(std::string const &filepath)
     while (available > 0) {
         size_t left = std::min(CHAR_BUF_SIZE, available);
         available -= left;
-        ifs.read(buf, CHAR_BUF_SIZE);
-        result += std::hash<std::string>{}(buf);
+        ifs.read(buf, left);
+        result += std::hash<std::string>{}(std::string(buf, left));
     }
     return result;
 }
 
-bool equal(std::pair<std::string, size_t> const &a,
-           std::pair<std::string, size_t> const &b)
+struct file_brief {
+    std::string filename;
+    size_t size;
+    size_t hash;
+
+    bool operator==(file_brief const &other) const
+    {
+        return other.hash == hash && other.size == size &&
+               other.filename == filename;
+    }
+};
+
+bool equal(file_brief const &a, file_brief const &b)
 {
-    if (a.second != b.second) {
+    if (a.hash != b.hash) {
         return false;
     }
-    if (fs::file_size(a.first) != fs::file_size(b.first)) {
+    if (a.size != b.size) {
         return false;
     }
-    size_t file_length = fs::file_size(a.first);
-    std::ifstream first_ifs(a.first, std::ios::binary),
-        second_ifs(b.first, std::ios::binary);
-    char first_buf[CHAR_BUF_SIZE + 1], second_buf[CHAR_BUF_SIZE + 1];
-    first_buf[CHAR_BUF_SIZE] = '\0';
-    second_buf[CHAR_BUF_SIZE] = '\0';
-    memset(first_buf, 0, CHAR_BUF_SIZE);
-    memset(second_buf, 0, CHAR_BUF_SIZE);
+    ++hashcheck;
+    std::ifstream first_ifs(a.filename, std::ios::binary),
+        second_ifs(b.filename, std::ios::binary);
+    char first_buf[CHAR_BUF_SIZE], second_buf[CHAR_BUF_SIZE];
+    size_t file_length = a.size;
     while (file_length > 0) {
         size_t left = std::min(CHAR_BUF_SIZE, file_length);
         file_length -= left;
         first_ifs.read(first_buf, CHAR_BUF_SIZE);
         second_ifs.read(second_buf, CHAR_BUF_SIZE);
-        if (strcmp(first_buf, second_buf) != 0) {
+        if (strncmp(first_buf, second_buf, left) != 0) {
             return false;
         }
     }
@@ -103,51 +116,87 @@ bool equal(std::pair<std::string, size_t> const &a,
 std::vector<std::vector<std::string>> static group_everything(
     std::string const &path, std::string const &root = "")
 {
-    clock_t begin = clock();
+    std::chrono::high_resolution_clock::time_point t1 =
+        std::chrono::high_resolution_clock::now();
     // Maybe std::vector will be better cause of small number of files
-    std::list<std::pair<std::string, size_t>> filenames;
     std::string directory = root == "" ? path : root;
-    for (std::string const &filename : get_filenames(directory)) {
-        filenames.push_back(make_pair(filename, get_hash(filename)));
+    std::vector<std::pair<std::string, size_t>> filenames =
+        get_filenames(directory);
+    std::unordered_map<size_t, size_t> occurrences;
+    for (auto &&file_and_size : filenames) {
+        ++occurrences[file_and_size.second];
+    };
+    std::list<file_brief> filtered_filenames;
+
+    std::chrono::high_resolution_clock::time_point tk =
+        std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(tk - t1).count();
+    std::cout << "[INFO] Basic parsing of " << directory << " finished in "
+              << duration << "ms" << std::endl;
+
+    for (auto &&file_and_size : filenames) {
+        if (occurrences[file_and_size.second] >= 2) {
+            filtered_filenames.push_back({file_and_size.first,
+                                          file_and_size.second,
+                                          get_hash(file_and_size.first)});
+        }
     }
+    // chrono stuff
+    std::chrono::high_resolution_clock::time_point tm =
+        std::chrono::high_resolution_clock::now();
+    duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(tm - t1).count();
+    std::cout << "[INFO] Hashing " << directory << " finished in " << duration
+              << "ms" << std::endl;
+    // chrono stuff
+
     std::vector<std::vector<std::string>> groups;
-    filenames.sort([](std::pair<std::string, size_t> const &a,
-                      std::pair<std::string, size_t> const &b) {
-        return a.second < b.second;
+    filtered_filenames.sort([](file_brief const &a, file_brief const &b) {
+        return a.size == b.size ? a.hash < b.hash : a.size < b.size;
     });
     if (root == "") {
-        while (!filenames.empty()) {
-            std::pair<std::string, size_t> file = *filenames.begin();
-            filenames.erase(filenames.begin());
-            groups.push_back(std::vector<std::string>{file.first});
-            std::vector<std::list<std::pair<std::string, size_t>>::iterator>
-                iters;
-            for (auto file_iter = filenames.begin();
-                 file_iter != filenames.end(); ++file_iter) {
-                if (file_iter->second != file.second) {
+        while (!filtered_filenames.empty()) {
+            file_brief file = *filtered_filenames.begin();
+            filtered_filenames.erase(filtered_filenames.begin());
+            groups.push_back(std::vector<std::string>{file.filename});
+            std::vector<std::list<file_brief>::iterator> iters;
+            for (auto file_iter = filtered_filenames.begin();
+                 file_iter != filtered_filenames.end(); ++file_iter) {
+                if (file_iter->hash != file.hash ||
+                    file_iter->size != file.size) {
                     break;
                 }
                 if (equal(*file_iter, file)) {
-                    groups.back().push_back(file_iter->first);
+                    groups.back().push_back(file_iter->filename);
                     iters.push_back(file_iter);
                 }
             }
+            if (groups.back().size() == 1) {
+                groups.pop_back();
+            }
             for (auto iter : iters) {
-                filenames.erase(iter);
+                filtered_filenames.erase(iter);
             }
         }
     } else {
-        auto mainfile = std::find(filenames.begin(), filenames.end(),
-                                  make_pair(path, get_hash(path)));
+        auto mainfile =
+            std::find(filtered_filenames.begin(), filtered_filenames.end(),
+                      file_brief{path, fs::file_size(path), get_hash(path)});
         groups.push_back({});
-        for (auto &&it : filenames) {
+        for (auto &&it : filtered_filenames) {
             if (equal(it, *mainfile)) {
-                groups.back().push_back(it.first);
+                groups.back().push_back(it.filename);
             }
         }
     }
-    std::cout << "[INFO] Scanning " << directory << " finished in "
-              << double(clock() - begin) / CLOCKS_PER_SEC << "s" << std::endl;
+    std::chrono::high_resolution_clock::time_point t2 =
+        std::chrono::high_resolution_clock::now();
+
+    duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+    std::cout << "[INFO] Scanning " << directory << " finished in " << duration
+              << "ms with " << hashcheck << " collisions" << std::endl;
     return groups;
 }
 } // namespace
--- HW05/test/checker_test.cpp
+++ HW05/test/checker_test.cpp
@@ -7,21 +7,49 @@
 #include <utility>
 #include <vector>
 
-#include "../include/checker.hpp"
+#include "../include/core/checker.hpp"
 
 namespace fs = std::filesystem;
 
 typedef std::vector<std::vector<std::string>> groups;
+
+void sort_answer(groups &actual)
+{
+    std::for_each(
+        actual.begin(), actual.end(), [](std::vector<std::string> &target) {
+            std::for_each(target.begin(), target.end(), [](std::string &value) {
+                value = fs::canonical(value);
+            });
+            std::sort(target.begin(), target.end());
+        });
+    auto comparator = [](std::vector<std::string> const &a,
+                         std::vector<std::string> const &b) {
+        for (size_t i = 0; i < std::min(a.size(), b.size()); ++i) {
+            if (a[i] != b[i]) {
+                return a[i] < b[i];
+            }
+        }
+        return a.size() < b.size();
+    };
+    std::sort(actual.begin(), actual.end(), comparator);
+}
 
 TEST(correctness, basic)
 {
     // Correctness of the ability to do something useful
     std::string directory = "./tmp/";
     fs::create_directory(directory);
+    //    directory = fs::canonical(directory);
     std::ofstream(directory + "a").write("green", sizeof("green"));
     groups res = core::group_all(directory);
+    ASSERT_EQ(groups{}, res);
     fs::remove_all(directory);
-    ASSERT_EQ(res, groups{{directory + "a"}});
+}
+
+std::ifstream::pos_type file_size(const char *filename)
+{
+    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+    return in.tellg();
 }
 
 TEST(correctness, two_inequal_files)
@@ -31,13 +59,16 @@ TEST(correctness, two_inequal_files)
     fs::create_directory(directory);
     std::string content1 = "green";
     std::string content2 = "red";
-    std::ofstream(directory + "a")
-        .write(content1.c_str(), sizeof(content1.c_str()));
-    std::ofstream(directory + "b")
-        .write(content2.c_str(), sizeof(content2.c_str()));
+    std::ofstream(directory + "a").write(content1.c_str(), 5);
+    std::string first_file = directory + "a";
+    std::cout << "-->" << file_size(first_file.c_str()) << std::endl;
+    std::ofstream(directory + "b").write(content2.c_str(), 3);
+    //    std::cout << "-->" << file_size(directory + "b") << std::endl;
     groups actual = core::group_all(directory);
+    groups expected{};
+    sort_answer(actual);
+    sort_answer(expected);
     fs::remove_all(directory);
-    groups expected{{directory + "a"}, {directory + "b"}};
     ASSERT_EQ(expected, actual);
 }
 
@@ -52,8 +83,10 @@ TEST(correctness, two_equal_files)
     std::ofstream(directory + "b")
         .write(content1.c_str(), sizeof(content1.c_str()));
     groups actual = core::group_all(directory);
-    fs::remove_all(directory);
     groups expected{{directory + "a", directory + "b"}};
+    sort_answer(actual);
+    sort_answer(expected);
+    fs::remove_all(directory);
     ASSERT_EQ(expected, actual);
 }
 
@@ -74,10 +107,11 @@ TEST(correctness, mixup)
                   [](std::vector<std::string> &target) {
                       std::sort(target.begin(), target.end());
                   });
-    fs::remove_all(directory);
     groups expected{{directory + "a", directory + "c", directory + "h"},
-                    {directory + "b", directory + "e"},
-                    {directory + "d"}};
+                    {directory + "b", directory + "e"}};
+    sort_answer(actual);
+    sort_answer(expected);
+    fs::remove_all(directory);
     ASSERT_EQ(expected, actual);
 }
 
@@ -119,21 +153,28 @@ TEST(correctness, mixup_big)
             .write(content.c_str(), sizeof(content.c_str()));
     }
     groups actual = core::group_all(directory);
-    std::for_each(actual.begin(), actual.end(),
-                  [](std::vector<std::string> &target) {
-                      std::sort(target.begin(), target.end());
-                  });
-    auto comparator = [](std::vector<std::string> const &a,
-                         std::vector<std::string> const &b) {
-        for (size_t i = 0; i < std::min(a.size(), b.size()); ++i) {
-            if (a[i] != b[i]) {
-                return a[i] < b[i];
-            }
-        }
-        return a.size() < b.size();
-    };
-    std::sort(actual.begin(), actual.end(), comparator);
-    std::sort(expected.begin(), expected.end(), comparator);
+
+    sort_answer(actual);
+    sort_answer(expected);
     fs::remove_all(directory);
     ASSERT_EQ(expected, actual);
+}
+
+TEST(time_measure, gradle)
+{
+    std::string directory = "/home/knisht/ITMO";
+    groups result = core::group_all(directory);
+
+    std::ofstream ofs("aasd");
+    for (auto &&it : result) {
+        for (auto &&jt : it) {
+            //            if (fs::file_size(jt) == 0 || jt.find("gradle") ==
+            //            jt.npos) {
+            //                continue;
+            //            }
+            ofs << jt << std::endl;
+        }
+        ofs << std::endl;
+    }
+    ASSERT_EQ(1, 1);
 }
